b8e9df2dbc6f136e18645903e303a93909ba0f4b
Cameron Pavey <cpave3@gmail.com>
2020-08-01 16:25:13 +1000

    basic frame

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
 interface Category {
     slug: string;
--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,25 +1,77 @@
-import React, { useEffect, useState } from "react";
+import React, { FunctionComponent } from "react";
 import Swiper from "react-id-swiper";
 import "swiper/swiper-bundle.css";
 import styled from "styled-components";
 
 import { categories } from "./Categories";
+import { RouteComponentProps, withRouter, Link } from "react-router-dom";
 
-const Feed = () => {
+import "./Feed.css";
+
+import { stories } from "../assets/stories";
+
+interface FeedRouterProps {
+  type: string;
+}
+
+interface Props extends RouteComponentProps<FeedRouterProps> {}
+
+const Feed: FunctionComponent<Props> = ({ match }) => {
   const Slide = styled.div`
-    background-color: "red";
-    height: 100vh;
+    border: 1px solid purple;
+    box-sizing: border-box;
+    /* height: 100vh; */
+    display: flex;
+    justify-content: center;
+    /* align-items: center; */
+    flex-direction: column;
+    height: 100% !important;
+  `;
+
+  const initialSlide = categories.findIndex(
+    (cat) => cat.slug === match?.params?.type
+  );
+
+  const Snip = styled.div`
+    border: 1px solid red;
+    box-sizing: border-box;
+    height: 50vh;
+  `;
+
+  const Sdiv = styled.div`
+    /* height: 100vh; */
   `;
 
   return (
-    <Swiper direction="vertical" initialSlide={0}>
-      {categories.map(({ slug, label, color }) => {
-        return <Slide key={slug} style={{ background: color }}>
-          {label}
-        </Slide>;
-      })}
-    </Swiper>
+    <Sdiv>
+      <Swiper
+        direction="vertical"
+        initialSlide={initialSlide}
+        containerClass="mySwiper"
+      >
+        {categories.map(({ slug, label, color }) => {
+          const snippets = stories[slug];
+
+          return (
+            <Slide key={slug} style={{ background: color }}>
+              <h1>{label}</h1>
+              <Swiper
+                direction="horizontal"
+                initialSlide={0}
+                containerClass="swiper2"
+              >
+                {snippets.map((snippet) => (
+                  <Snip>
+                    <p>{snippet}</p>
+                  </Snip>
+                ))}
+              </Swiper>
+            </Slide>
+          );
+        })}
+      </Swiper>
+    </Sdiv>
   );
 };
 
-export default Feed;
+export default withRouter(Feed);
